c0b70c0c830b9cab692845007baf7882fabb2737
raluca-elena <ralucaelena1985@gmail.com>
2013-08-13 11:01:17 +0300

    client / server two channel implementation -- one for commands and one for imager. at this poit for each image we instantiate a new connaction.

--- client.py
+++ client.py
@@ -4,24 +4,33 @@ import socket
 import time
 import struct
 import urllib2
+import random
 TCP_IP = '127.0.0.1'
 
 TCP_PORT = 6666
 BUFFER_SIZE = 40
-MESSAGE = "aloha"
+my_name = random.randint(2000, 3000)
+#MESSAGE = "aloha"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+s.send(struct.pack("I", my_name))
 
-data = s.recv(BUFFER_SIZE)
+
 #s.setblocking(0)
 print('la inceput in client')
-while(len(data)) :
-        someType = urllib2.urlopen("http://192.168.1.6:8080/?action=snapshot").read()
-        MESSAGE = someType
-        s.sendall(struct.pack("I", len(MESSAGE)))
-        s.sendall(MESSAGE)
-#s.close()
+while True:
+        data = s.recv(BUFFER_SIZE)
+        new_port = struct.unpack("I", data)[0]
+        print('received new port', new_port)
+        s_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s_data.connect((TCP_IP, new_port))
+        print ('just instantiated new socket s_data')
+
+        imgData = urllib2.urlopen("http://192.168.1.6:8080/?action=snapshot").read()
+
+
+        s_data.sendall(imgData)
+        s_data.close()
 
 
 
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 import socket
 import struct
+import random
+import logging
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 BUFFER_SIZE = 9948       
 class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -8,25 +10,37 @@ class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_home(self):
         try:
             print('am intrat in do home')
-            conn.send("give me data")
-            data = conn.recv(4)
-            assert len(data) == 4
-            len_data = struct.unpack("I", data)[0]
-            data = conn.recv(len_data)
-            assert len(data) == len_data 
-           
-            #if not data: break
+            #data = conn.recv(4)
+            #no_placa = struct.unpack("I", data)[0]
+            
+            TCP_PORT = random.randint(6667, 8887)
+            print ('the port is ' , TCP_PORT)
+
+            s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s1.bind((TCP_IP, TCP_PORT))
+            s1.listen(1)
+            command_socket.sendall(struct.pack("I", TCP_PORT))
+            print ('i have sent the posrt from server')
+
+            conn1, addr1 = s1.accept()
             #send code 200 response
             self.send_response(200)
             #send header first
             self.send_header('Content-type','image/jpeg')
             self.end_headers()
+            
+            while True:
+                data1 = conn1.recv(BUFFER_SIZE)
+                if not data1: break
+                #send file content to client
+                self.wfile.write(data1)
+                break
+            
 
-            #send file content to client
-            self.wfile.write(data)
-
-        except IOError as e:
-            self.send_error(404, 'file not found: ' + str(e))
+        except Exception as e:
+            logging.exception("I HATE AVWEONE!")
+            self.send_error(505, 'file not found: ' + str(e))
 
          
     #handle GET command
@@ -39,19 +53,22 @@ class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
        
 def run():       
     print('la inceput in server')
+    global TCP_IP
     TCP_IP = '127.0.0.1'
     TCP_PORT = 6666
     #BUFFER_SIZE = 20
-    global s
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
     s.bind((TCP_IP, TCP_PORT))
     s.listen(1)
 
-    global conn
-    conn, addr = s.accept()
-    data = conn.recv(BUFFER_SIZE)
+    global command_socket
+    command_socket, addr = s.accept()
+    data = command_socket.recv(BUFFER_SIZE)
+    nr_placa = struct.unpack("I", data)[0]
+    print ('tocmai am primit numarul placii care este: ' ,  nr_placa)
+        
 
     server_address = ('127.0.0.1', 8888)
     httpd = HTTPServer(server_address, KodeFunHTTPRequestHandler)
