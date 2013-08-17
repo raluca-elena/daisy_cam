84bc87e3366839395c53787997bd8d57c153849b
raluca-elena <ralucaelena1985@gmail.com>
2013-08-17 16:54:34 +0300

    added functionality for checking/removing disconected boards

--- client.py
+++ client.py
@@ -9,6 +9,19 @@ import random
 TCP_IP = '127.0.0.1'
 #TCP_IP = 'daysi-cam-23675.euw1.actionbox.io'
 
+def set_keepalive(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+
+
 TCP_PORT = 6666
 BUFFER_SIZE = 40
 
@@ -18,6 +31,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
 nr_bytes = s.send(struct.pack("I", my_name))
+set_keepalive(s, after_idle_sec=1, interval_sec=3, max_fails=5)
+         
 print 'sending id_placa: ', nr_bytes
 print('running client')
 while True:
@@ -28,9 +43,10 @@ while True:
 
         s_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s_data.connect((TCP_IP, new_port))
+        
         print ('just instantiated new socket s_data')
         start = time.time()
-        imgData = urllib2.urlopen("http://192.168.1.6:8080/?action=snapshot").read()
+        imgData = urllib2.urlopen("http://192.168.1.3:8080/?action=snapshot").read()
         print "this lasted  ---------: " , time.time() - start
 
 
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import struct
 import random
 import logging
 import thread
+import time
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 BUFFER_SIZE = 9948   
@@ -138,8 +139,7 @@ MICU RULZ!
         print ('i have sent the port from server')
         conn1, addr1 = s1.accept()
         return conn1
-     
-         
+                 
     #handle GET command
     def do_GET(self):
         print('the path from get is: '+ self.path)
@@ -157,17 +157,51 @@ MICU RULZ!
         else :         
                 self.send_error(404, 'file not found: ' + self.path)
 
+def set_keepalive(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
 
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+          
+def verify_conn():
+    print 'just entered in board verification!'
+    global conn_pool
+    print conn_pool
+    while True:
+        to_delete = []
+        for (k, v) in conn_pool.items():
+            if  not is_alive(v):
+                to_delete.append(k)
+        for k in to_delete:
+            del conn_pool[k]
+        time.sleep(1)
+
+def is_alive(sock):
+    try:
+        response = sock.recv(1)
+    except socket.error:
+        return True
+    return False
+    
 def initiate_board_conn():
     TCP_PORT = 6666
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    set_keepalive(s, after_idle_sec=1, interval_sec=3, max_fails=5)
     s.bind((TCP_IP, TCP_PORT))
     s.listen(1)
     while True:    
         command_socket, addr = s.accept()
+        set_keepalive(command_socket, after_idle_sec=1, interval_sec=3, max_fails=5)
         data = command_socket.recv(BUFFER_SIZE)
         nr_placa = struct.unpack("I", data)[0]
+        command_socket.setblocking(0)
         print ('just got the id of the board that is: ' ,  nr_placa)
         global conn_pool
         conn_pool[nr_placa]= command_socket
@@ -176,7 +210,7 @@ def initiate_board_conn():
 def run():       
     print('at the beginning of the server side: ')
     thread.start_new_thread( initiate_board_conn , () )
-    
+    thread.start_new_thread(verify_conn, ())
     server_address = ('0.0.0.0', 8989)
     httpd = HTTPServer(server_address, KodeFunHTTPRequestHandler)
     print('http server is running...')
